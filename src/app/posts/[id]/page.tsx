80a5da6bda4c1dba9cc095d3fbbc4cf32302c144
ykNe0372 <130669282+ykNe0372@users.noreply.github.com>
2025-01-22 17:29:12 +0900

    実装漏れがあったので実装

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,37 +1,59 @@
 "use client";
 import { useState, useEffect } from "react";
-import { useParams } from "next/navigation";
-import dayjs from "dayjs";
+import { useParams } from "next/navigation"; // ◀ 注目
+
 import type { Post } from "@/app/_types/Post";
-import dummyPosts from "@/app/_mocks/dummyPosts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
+
 import DOMPurify from "isomorphic-dompurify";
 
 // 投稿記事の詳細表示 /posts/[id]
 const Page: React.FC = () => {
   const [post, setPost] = useState<Post | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const dtFmt = "YYYY-MM-DD";
+  const [fetchError, setFetchError] = useState<string | null>(null);
 
-  // 動的ルートパラメータから記事 id を取得
+  // 動的ルートパラメータから id を取得 （URL:/posts/[id]）
   const { id } = useParams() as { id: string };
 
-  // コンポーネントが読み込まれた時一回だけ実行する
+  const apiBaseEp = process.env.NEXT_PUBLIC_MICROCMS_BASE_EP!;
+  const apiKey = process.env.NEXT_PUBLIC_MICROCMS_API_KEY!;
+
   useEffect(() => {
-    setIsLoading(true); // モックデータを使用 (API 叩くのはもうちょい先)
-    const timer = setTimeout(() => {
-      console.log("ウェブ API からデータを取得しました (してない)");
-      setPost(dummyPosts.find((post) => post.id === id) || null); // dummyPosts から id に一致する投稿を取得➡セット
-      setIsLoading(false);
-    }, 1000);
+    const fetchPosts = async () => {
+      setIsLoading(true);
+      try {
+        const requestUrl = `${apiBaseEp}/posts/${id}`;
+        const response = await fetch(requestUrl, {
+          method: "GET",
+          cache: "no-store",
+          headers: {
+            "X-MICROCMS-API-KEY": apiKey,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("データの取得に失敗しました");
+        }
+        const data = await response.json();
+        setPost(data as Post);
+      } catch (e) {
+        setFetchError(
+          e instanceof Error ? e.message : "予期せぬエラーが発生しました"
+        );
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPosts();
+  }, [apiBaseEp, apiKey, id]);
 
-    // データ取得の途中でページ遷移した時にタイマーを解除する
-    return () => clearTimeout(timer);
-  }, [id]);
+  if (fetchError) {
+    return <div>{fetchError}</div>;
+  }
 
-  // 投稿データの取得中は Loading...
+  // 投稿データの取得中は「Loading...」を表示
   if (isLoading) {
     return (
       <div className="text-gray-500">
@@ -41,12 +63,12 @@ const Page: React.FC = () => {
     );
   }
 
-  // 投稿データが取得できなければエラーを表示
+  // 投稿データが取得できなかったらエラーメッセージを表示
   if (!post) {
-    return <div>指定 id の投稿の取得に失敗しました。</div>;
+    return <div>指定idの投稿の取得に失敗しました。</div>;
   }
 
-  // HTML コンテンツのサニタイズ
+  // HTMLコンテンツのサニタイズ
   const safeHTML = DOMPurify.sanitize(post.content, {
     ALLOWED_TAGS: ["b", "strong", "i", "em", "u", "br"],
   });
@@ -54,7 +76,6 @@ const Page: React.FC = () => {
   return (
     <main>
       <div className="space-y-2">
-        <div>{dayjs(post.createdAt).format(dtFmt)}</div>
         <div className="mb-2 text-2xl font-bold">{post.title}</div>
         <div>
           <Image
@@ -66,7 +87,7 @@ const Page: React.FC = () => {
             className="rounded-xl"
           />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: safeHTML }}></div>
+        <div dangerouslySetInnerHTML={{ __html: safeHTML }} />
       </div>
     </main>
   );
