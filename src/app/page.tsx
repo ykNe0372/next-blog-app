9e70f3e53a4289c9ccba64ab1dbbdc668a64f0a8
ykNe0372 <130669282+ykNe0372@users.noreply.github.com>
2025-02-20 17:59:46 +0900

    ソート機能を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import type { Post } from "@/app/_types/Post";
 import PostSummary from "@/app/_components/PostSummary";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons/faCaretUp";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons/faCaretDown";
 import { PostApiResponse } from "./_types/PostApiResponse";
 import { useAuth } from "./_hooks/useAuth";
 import Link from "next/link";
@@ -12,6 +15,8 @@ import { twMerge } from "tailwind-merge";
 const Page: React.FC = () => {
   const [posts, setPosts] = useState<Post[] | null>(null);
   const [fetchError, setFetchError] = useState<string | null>(null);
+  const [sortByCreatedAt, setSortByCreatedAt] = useState<boolean>(false);
+  const [isDescending, setIsDescending] = useState<boolean>(true); // ソート順を管理する状態変数
 
   const { session } = useAuth();
 
@@ -48,6 +53,19 @@ const Page: React.FC = () => {
     };
     fetchPosts();
   }, []);
+
+  const handleSortByCreatedAt = () => {
+    if (posts) {
+      const sortedPosts = [...posts].sort((a, b) => {
+        const dateA = new Date(a.createdAt).getTime();
+        const dateB = new Date(b.createdAt).getTime();
+        return isDescending ? dateB - dateA : dateA - dateB;
+      });
+      setPosts(sortedPosts);
+      setSortByCreatedAt(true);
+      setIsDescending(!isDescending); // ソート順を切り替える
+    }
+  };
 
   if (fetchError) {
     return <div>{fetchError}</div>;
@@ -86,18 +104,15 @@ const Page: React.FC = () => {
       <div className="mb-4 flex items-center gap-x-2">
         <button
           type="button"
+          onClick={handleSortByCreatedAt}
           className={twMerge(
             "my-2 rounded-md border border-slate-700 bg-gray-100 px-4 py-1 font-bold text-slate-700 hover:bg-gray-300"
           )}
         >
-          タイトル名でソート
-        </button>
-        <button
-          type="button"
-          className={twMerge(
-            "my-2 rounded-md border border-slate-700 bg-gray-100 px-4 py-1 font-bold text-slate-700 hover:bg-gray-300"
-          )}
-        >
+          <FontAwesomeIcon
+            icon={isDescending ? faCaretDown : faCaretUp}
+            className="mr-1"
+          />
           投稿日時でソート
         </button>
       </div>
@@ -117,7 +132,10 @@ const Page: React.FC = () => {
       </div>
       <div className="space-y-3">
         {posts.map((post) => (
-          <PostSummary key={post.id} post={post} />
+          <PostSummary
+            key={sortByCreatedAt ? post.createdAt : post.id}
+            post={post}
+          />
         ))}
       </div>
     </main>
