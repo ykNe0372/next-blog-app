3b62240a0e94318517262a0f5ad72bcc04788b89
ykNe0372 <130669282+ykNe0372@users.noreply.github.com>
2025-02-20 09:37:46 +0900

    カテゴリ名表示

--- src/app/_components/PostSummary.tsx
+++ src/app/_components/PostSummary.tsx
@@ -1,6 +1,7 @@
 "use client";
 import dayjs from "dayjs";
 import type { Post } from "@/app/_types/Post";
+import { twMerge } from "tailwind-merge";
 import Link from "next/link";
 
 type Props = {
@@ -13,7 +14,25 @@ const PostSummary: React.FC<Props> = (props) => {
   return (
     <Link href={`/posts/${post.id}`}>
       <div className="mb-4 border border-slate-400 p-3">
-        <div>{dayjs(post.createdAt).format(dtFmt)}</div>
+        <div className="flex items-center justify-between">
+          <div>{dayjs(post.createdAt).format(dtFmt)}</div>
+          <div className="flex space-x-1.5">
+            {post.categories.map((category) => (
+              <div
+                key={category.id}
+                className={twMerge(
+                  "rounded-md px-2 py-0.5",
+                  "text-xs font-bold",
+                  "border border-slate-400 text-slate-500"
+                )}
+              >
+                <Link href={`/admin/categories/${category.id}`}>
+                  {category.name}
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
         <div className="font-bold">{post.title}</div>
         {/* <div>{post.content}</div> */}
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
